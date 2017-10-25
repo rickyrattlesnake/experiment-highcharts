944e4863018048ab4ca604e27b8b331575f16ac2
rickyrattlesnake <ricky.ratnayake@gmail.com>
2017-10-25 22:12:27 +1100

    highcharts working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
+import { chart } from 'highcharts';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('chartTarget') chartTarget: ElementRef;
   title = 'app';
+  chart: Highcharts.ChartObject;
+
+  ngAfterViewInit() {
+    const options: Highcharts.Options = {
+      chart: {
+        type: 'bar'
+      },
+      title: {
+        text: 'Fruit Consumption'
+      },
+      xAxis: {
+        categories: ['Apples', 'Bananas', 'Oranges']
+      },
+      yAxis: {
+        title: {
+          text: 'Fruit eaten'
+        }
+      },
+      series: [{
+        name: 'Jane',
+        data: [1, 0, 4]
+      }, {
+        name: 'John',
+        data: [5, 7, 3]
+      }]
+    };
+
+    this.chart = chart(this.chartTarget.nativeElement, options);
+  }
+
+  ngOnDestroy() {
+    this.chart = null;
+  }
+
 }
