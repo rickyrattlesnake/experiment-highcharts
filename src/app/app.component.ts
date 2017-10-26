7aa8d3fd1b0171aa288484a3a3482019cdc9bd12
rickyrattlesnake <ricky.ratnayake@gmail.com>
2017-10-26 23:25:28 +1100

    live data sparkline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,46 +1,146 @@
 import { Component, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
-import { chart } from 'highcharts';
+import * as HC from 'highcharts';
+const Highcharts: any = HC;
+
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/delay';
+import 'rxjs/add/operator/concatAll';
+import 'rxjs/add/observable/of';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements AfterViewInit, OnDestroy {
-  @ViewChild('chartTarget') chartTarget: ElementRef;
+  @ViewChild('sparkLine') sparkLineTarget: ElementRef;
+
   title = 'app';
-  chart: Highcharts.ChartObject;
+  sparkLineChart: Highcharts.ChartObject;
+
+  testData = {
+    name: 'Test',
+    data: [1, 0, 4, 10, 100, 25, 100, 302, 10, 1, 0, 4, 10, 100, 25, 100, 302, 10]
+  };
 
   ngAfterViewInit() {
-    const options: Highcharts.Options = {
-      chart: {
-        type: 'bar'
-      },
-      title: {
-        text: 'Fruit Consumption'
-      },
-      xAxis: {
-        categories: ['Apples', 'Bananas', 'Oranges']
-      },
-      yAxis: {
-        title: {
-          text: 'Fruit eaten'
-        }
-      },
-      series: [{
-        name: 'Jane',
-        data: [1, 0, 4]
-      }, {
-        name: 'John',
-        data: [5, 7, 3]
-      }]
-    };
+    this.sparkLineChart = this.renderSparkLine();
 
-    this.chart = chart(this.chartTarget.nativeElement, options);
+    this.addChartData()
+      .subscribe(() => {
+        console.log('Added DataPoint');
+        this.sparkLineChart.redraw();
+      });
   }
 
   ngOnDestroy() {
-    this.chart = null;
+    this.sparkLineChart = null;
   }
 
+  renderSparkLine() {
+    return Highcharts.chart(this.sparkLineTarget.nativeElement, {
+        chart: {
+          backgroundColor: null,
+          borderWidth: 0,
+          type: 'spline',
+          margin: [2, 0, 2, 0],
+          width: 120,
+          height: 20,
+          style: {
+            overflow: 'visible'
+          },
+
+          // small optimalization, saves 1-2 ms each sparkline
+          skipClone: true
+        },
+        title: {
+            text: ''
+        },
+        credits: {
+            enabled: false
+        },
+        xAxis: {
+            labels: {
+                enabled: false
+            },
+            title: {
+                text: null
+            },
+            startOnTick: false,
+            endOnTick: false,
+            tickPositions: []
+        },
+        yAxis: {
+            endOnTick: false,
+            startOnTick: false,
+            labels: {
+                enabled: false
+            },
+            title: {
+                text: null
+            },
+            tickPositions: [0]
+        },
+        legend: {
+            enabled: false
+        },
+        tooltip: {
+            backgroundColor: null,
+            borderWidth: 0,
+            shadow: false,
+            useHTML: true,
+            hideDelay: 0,
+            shared: true,
+            padding: 0,
+            positioner: function (w, h, point) {
+                return { x: point.plotX - w / 2, y: point.plotY - h };
+            }
+        },
+        plotOptions: {
+            series: {
+                animation: false,
+                lineWidth: 1,
+                shadow: false,
+                states: {
+                    hover: {
+                        lineWidth: 1
+                    }
+                },
+                marker: {
+                    radius: 1,
+                    states: {
+                        hover: {
+                            radius: 2
+                        }
+                    }
+                },
+                fillOpacity: 0.25
+            },
+            column: {
+                negativeColor: '#910000',
+                borderColor: 'silver'
+            }
+      },
+      series: [
+        {
+          name: 'Test',
+          data: []
+        },
+      ]
+    });
+  }
+
+  addChartData() {
+    return Observable.of(...this.testData.data)
+    .map(d => Observable.of(d).delay(2000))
+    .concatAll()
+    .map(d => {
+      this.sparkLineChart.series[0].addPoint(d, false, false, {
+        duration: 100,
+      });
+    });
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { applyHighchartsTheme } from './app/highcharts-theme';
+
 if (environment.production) {
   enableProdMode();
 }
